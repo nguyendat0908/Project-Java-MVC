d791f02b42465885c5810f5d28e74ecc8e727010
Nguyendat0908 <Nguyendat090803@gmail.com>
2024-08-14 22:58:08 +0700

    Send Data to HTML Form

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -3,8 +3,13 @@ package vn.hoidanit.laptopshop.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
+import vn.hoidanit.laptopshop.domain.User;
 import vn.hoidanit.laptopshop.service.UserService;
+
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class UserController {
@@ -25,9 +30,17 @@ public class UserController {
     }
 
     @RequestMapping("/admin/user")
-    public String getUserPage() {
+    public String getUserPage(Model model) {
+        model.addAttribute("newUser", new User());
         return "admin/user/create";
     }
+
+    @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
+    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        System.out.println("Run here!" +hoidanit);
+        return "Hello";
+    }
+    
 
 }
 
