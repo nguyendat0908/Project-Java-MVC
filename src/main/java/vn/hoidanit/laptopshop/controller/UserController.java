b7152c322f7c2ac0a1a520af8defa606454dee96
Nguyendat0908 <Nguyendat090803@gmail.com>
2024-08-15 01:10:26 +0700

    Function Create User

--- src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
+++ src/main/java/vn/hoidanit/laptopshop/controller/UserController.java
@@ -4,17 +4,17 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 
 import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 import vn.hoidanit.laptopshop.service.UserService;
 
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-
 @Controller
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     // Dependency Injection - DI
     public UserController(UserService userService) {
@@ -23,8 +23,7 @@ public class UserController {
 
     @RequestMapping("/")
     public String getHomePage(Model model) {
-        String test = this.userService.handleHello();
-        model.addAttribute("dat", test);
+        model.addAttribute("dat", "test");
         model.addAttribute("datdeptrai", "Dat dep trai nhat trai dat!");
         return "hello";
     }
@@ -36,11 +35,8 @@ public class UserController {
     }
 
     @RequestMapping(value = "/admin/user/create", method = RequestMethod.POST)
-    public String createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
-        System.out.println("Run here!" +hoidanit);
-        return "Hello";
+    public User createUserPage(Model model, @ModelAttribute("newUser") User hoidanit) {
+        return this.userService.handleSaveUser(hoidanit);
     }
-    
 
 }
-
--- src/main/java/vn/hoidanit/laptopshop/service/UserService.java
+++ src/main/java/vn/hoidanit/laptopshop/service/UserService.java
@@ -1,13 +1,24 @@
 package vn.hoidanit.laptopshop.service;
 
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
+
+import vn.hoidanit.laptopshop.domain.User;
+import vn.hoidanit.laptopshop.repository.UserRepository;
 
 @Service
 public class UserService {
 
-    @GetMapping("")
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public String handleHello(){
         return "Hello from Service";
+    }
+
+    public User handleSaveUser(User user){
+        return this.userRepository.save(user);
     }
 }
