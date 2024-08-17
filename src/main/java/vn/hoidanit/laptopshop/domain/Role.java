933bf1b31173906633ba0a581370b8e48c676eab
Nguyendat0908 <Nguyendat090803@gmail.com>
2024-08-17 17:33:25 +0700

    Set relation OneToMany And ManyToOne

--- src/main/java/vn/hoidanit/laptopshop/domain/Role.java
+++ src/main/java/vn/hoidanit/laptopshop/domain/Role.java
@@ -1,22 +1,28 @@
 package vn.hoidanit.laptopshop.domain;
 
+import java.util.List;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "roles")
 public class Role {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     private String name;
     private String description;
 
-    
+    // Role one -> to many -> Users
+    @OneToMany(mappedBy = "role")
+    private List<User> users;
+
     public long getId() {
         return id;
     }
@@ -45,5 +51,5 @@ public class Role {
     public String toString() {
         return "Role [id=" + id + ", name=" + name + ", description=" + description + "]";
     }
-    
+
 }
